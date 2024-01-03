3dedf2377e34ef92e2f38eaface0bd7d0f0dda7a
Vince Cedie Pino <vincecediepino@gmail.com>
2024-01-03 09:39:08 +0700

    Finish Project

--- js/index.js
+++ js/index.js
@@ -1,30 +1,26 @@
 const imageUrl = window.location.origin + "/assets/images/";
-const icon = document.querySelectorAll('.icon');
+const summary = document.querySelectorAll('summary');
 
-icon.forEach((iconEl) => {
-  iconEl.addEventListener('click', () => {
+summary.forEach((summaryEl) => {
+  summaryEl.addEventListener('click', () => {
     // Close other elements
-    const allIcons = document.querySelectorAll('.icon');
-    allIcons.forEach((otherIcon) => {
-      if (otherIcon !== iconEl) {
-        otherIcon.src = `${imageUrl}icon-plus.svg`;
-        const otherQA = otherIcon.closest('.qa');
-        const otherAnswer = otherQA.querySelector('.answer');
-        otherAnswer.classList.add('hide');
+    const allSummaries = document.querySelectorAll('summary');
+    allSummaries.forEach((otherSummary) => {
+      if (otherSummary !== summaryEl) {
+        const otherQa = otherSummary.closest('.qa');
+        const imgUrl = otherQa.querySelector('img');
+        imgUrl.src = `${imageUrl}icon-plus.svg`;
+        otherQa.open = false;
       }
     });
 
-    // Open/close the clicked element
-    if (iconEl.src === `${imageUrl}icon-plus.svg`) {
-      iconEl.src = `${imageUrl}icon-minus.svg`;
-      const qa = iconEl.closest('.qa');
-      const answer = qa.querySelector('.answer');
-      answer.classList.remove('hide');
-    } else {
-      iconEl.src = `${imageUrl}icon-plus.svg`;
-      const qa = iconEl.closest('.qa');
-      const answer = qa.querySelector('.answer');
-      answer.classList.add('hide');
+    const qa = summaryEl.closest('.qa');
+    const imgUrl = qa.querySelector('img');
+    if (imgUrl.src === `${imageUrl}icon-plus.svg`) {
+      imgUrl.src = `${imageUrl}icon-minus.svg`;
+    }
+    else {
+      imgUrl.src = `${imageUrl}icon-plus.svg`;
     }
   });
 });
